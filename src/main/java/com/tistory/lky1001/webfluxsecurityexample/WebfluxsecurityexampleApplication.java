e02dc90db58533cea1ec53cc79872b2e6946bfdd
kylee <kylee@devband.com>
2018-11-06 22:50:50 +0900

    create project

---
+++ src/main/java/com/tistory/lky1001/webfluxsecurityexample/WebfluxsecurityexampleApplication.java
@@ -0,0 +1,12 @@
+package com.tistory.lky1001.webfluxsecurityexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxsecurityexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxsecurityexampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tistory/lky1001/webfluxsecurityexample/WebfluxsecurityexampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tistory.lky1001.webfluxsecurityexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebfluxsecurityexampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
